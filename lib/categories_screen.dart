5417581f2d99d0cec6ea28a3148cd287a4cce457
George <george@pop-os.localdomain>
2020-04-30 12:24:19 +0100

    scaffold page

--- lib/categories_screen.dart
+++ lib/categories_screen.dart
@@ -10,6 +10,7 @@ class CategoriesScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text('DeliMeal'),),
       body: GridView(
+        padding: const EdgeInsets.all(25),
         children: DUMMY_CATEGORIES
             .map((catData) => CategoryItem(
                   title: catData.title,
