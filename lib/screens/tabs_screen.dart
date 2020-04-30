2a1a7c173ba0c33582b700afd3251ccdcd6621d2
George <george@pop-os.localdomain>
2020-04-30 16:15:54 +0100

    completed navbar navigation

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -13,9 +13,9 @@ class TabsScreen extends StatefulWidget {
 
 class _TabsScreenState extends State<TabsScreen> {
 
-  final List<Widget> _pages = [
-    CategoriesScreen(),
-    FavouritesScreen(),
+  final List<Map<String, Object>> _pages = [
+    {'page' : CategoriesScreen(), 'title': 'Categories'},
+    {'page' : FavouritesScreen(), 'title': 'Favourites'},
   ];
   int _selectedPageIndex = 0;
 
@@ -29,9 +29,9 @@ class _TabsScreenState extends State<TabsScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Meals'),
+        title: Text(_pages[_selectedPageIndex]['title']),
       ),
-      body: _pages[_selectedPageIndex],
+      body: _pages[_selectedPageIndex]['page'],
       bottomNavigationBar: BottomNavigationBar(
         onTap: _selectPage,
         backgroundColor: Theme.of(context).primaryColor,
