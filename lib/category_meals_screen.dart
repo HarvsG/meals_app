a4d0b4a2ceff10535cf6af4a9c5019f65965f2c1
George <george@pop-os.localdomain>
2020-04-30 13:28:07 +0100

    builder now presents meals

--- lib/category_meals_screen.dart
+++ lib/category_meals_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './dummy_data.dart';
 
 class CategoryMealsScreen extends StatelessWidget {
   static const routeName = '/category-meals';
@@ -12,12 +13,17 @@ class CategoryMealsScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final routeArgs = ModalRoute.of(context).settings.arguments as Map<String, String>;
     final categoryTitle = routeArgs['title'];
-    //final categoryId = routeArgs['id'];
+    final categoryId = routeArgs['id'];
+    final categoryMeals = DUMMY_MEALS.where((meal){
+      return meal.categories.contains(categoryId);
+    }).toList();
 
     return Scaffold(
         appBar: AppBar(title: Text(categoryTitle)),
         body: Center(
-          child: Text('The recipies for the category'),
+          child: ListView.builder(itemBuilder: (ctx, index){
+            return Text(categoryMeals[index].title);
+          },itemCount: categoryMeals.length,),
         ));
   }
 }
